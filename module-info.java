adeed71491745ffbbf35bf74dbc545e659b44e26
DarinaKhizhnyak <145911568+DarinaKhizhnyak@users.noreply.github.com>
2023-11-05 02:12:49 +0300

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module org.nanotubes {
-    requires javafx.controls;
-    requires java.desktop;
-    exports org.nanotubes;
-    exports org.nanotubes.generation;
-    exports org.nanotubes.generation.PoissonDisk;
-    exports org.nanotubes.generation.Geom;
-    exports org.nanotubes.generation.Mapping;
-}
